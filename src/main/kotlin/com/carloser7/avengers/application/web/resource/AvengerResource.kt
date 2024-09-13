24e15ef0f8c79bfb5f697048b1b1a78a29d27235
CarlosEReis <carlos.er7@gmail.com>
2024-09-13 19:45:56 -0300

    refactor: changed the get  detail path

--- src/main/kotlin/com/carloser7/avengers/application/web/resource/AvengerResource.kt
+++ src/main/kotlin/com/carloser7/avengers/application/web/resource/AvengerResource.kt
@@ -32,7 +32,7 @@ class AvengerResource (
             ResponseEntity.ok().body(it)
         }
 
-    @GetMapping("{id}")
+    @GetMapping("{id}/detail")
     fun getAvengersDetails(@PathVariable("id") id: Long) =
         repository.getDetail(id)?.let {
             ResponseEntity.ok().body(AvengerResponse.from(it))
